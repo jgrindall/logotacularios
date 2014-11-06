6105f7909dd6520b9690f9514094e5369f78b154
John <john.grindall@gmail.com>
2014-11-06 15:47:57 +0000

    New files

---
+++ LogotacularIOS/SymmNotifications.h
@@ -0,0 +1,40 @@
+//
+//  SymmNotifications.h
+//  Symmetry
+//
+//  Created by John on 19/05/2014.
+//  Copyright (c) 2014 jgrindall. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SymmNotifications : NSObject
+
+extern NSString* const SYMM_NOTIF_SHOW_SPINNER;
+extern NSString* const SYMM_NOTIF_HIDE_SPINNER;
+extern NSString* const SYMM_NOTIF_CLICK_MENU;
+extern NSString* const SYMM_NOTIF_HIDE_MENU;
+extern NSString* const SYMM_NOTIF_CLICK_PLAY;
+extern NSString* const SYMM_NOTIF_START;
+extern NSString* const SYMM_NOTIF_STORE_TEXT;
+extern NSString* const SYMM_NOTIF_STOP;
+extern NSString* const SYMM_NOTIF_UNDO;
+extern NSString* const SYMM_NOTIF_RESET;
+extern NSString* const SYMM_NOTIF_REDO;
+extern NSString* const SYMM_NOTIF_LOAD;
+extern NSString* const SYMM_NOTIF_SAVE;
+extern NSString* const SYMM_NOTIF_CLEAR;
+extern NSString* const SYMM_NOTIF_CMD_RECEIVED;
+extern NSString* const SYMM_NOTIF_CLICK_STOP;
+extern NSString* const SYMM_NOTIF_DISMISS_KEY;
+extern NSString* const SYMM_NOTIF_CLICK_OPEN;
+extern NSString* const SYMM_NOTIF_LOAD_FILES;
+extern NSString* const SYMM_NOTIF_PERFORM_OPEN;
+extern NSString* const SYMM_NOTIF_PERFORM_DEL;
+extern NSString* const SYMM_NOTIF_CLICK_SAVE;
+extern NSString* const SYMM_NOTIF_SHOW_FILENAME;
+extern NSString* const SYMM_NOTIF_PERFORM_SAVE;
+extern NSString* const SYMM_NOTIF_TEXT_EDITED;
+
+@end
+
