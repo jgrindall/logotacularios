2d7dc102bba1d106beb78dca14d9b99bf055eac6
John <john.grindall@gmail.com>
2017-03-18 12:54:25 +0000

    New images

---
+++ imgs/list copy.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+convert -composite ./IMG_0187.PNG watermark.png -gravity center ./IMG_0187.PNG
+
---
+++ imgs/list.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+convert -composite ./IMG_0188.PNG watermark.png -gravity center ./IMG_0188.PNG
+convert -composite ./IMG_0189.PNG watermark.png -gravity center ./IMG_0189.PNG
+convert -composite ./IMG_0190.PNG watermark.png -gravity center ./IMG_0190.PNG
+convert -composite ./IMG_0192.PNG watermark.png -gravity center ./IMG_0192.PNG
+convert -composite ./IMG_0193.PNG watermark.png -gravity center ./IMG_0193.PNG
+convert -composite ./IMG_0194.PNG watermark.png -gravity center ./IMG_0194.PNG
+convert -composite ./IMG_0195.PNG watermark.png -gravity center ./IMG_0195.PNG
+convert -composite ./IMG_0196.PNG watermark.png -gravity center ./IMG_0196.PNG
+convert -composite ./IMG_0197.PNG watermark.png -gravity center ./IMG_0197.PNG
+convert -composite ./IMG_0198.PNG watermark.png -gravity center ./IMG_0198.PNG
+convert -composite ./IMG_0200.PNG watermark.png -gravity center ./IMG_0200.PNG
+convert -composite ./IMG_0201.PNG watermark.png -gravity center ./IMG_0201.PNG
+convert -composite ./IMG_0202.PNG watermark.png -gravity center ./IMG_0202.PNG
+convert -composite ./IMG_0203.PNG watermark.png -gravity center ./IMG_0203.PNG
+convert -composite ./IMG_0204.PNG watermark.png -gravity center ./IMG_0204.PNG
+convert -composite ./IMG_0205.PNG watermark.png -gravity center ./IMG_0205.PNG
+convert -composite ./IMG_0206.PNG watermark.png -gravity center ./IMG_0206.PNG
+convert -composite ./IMG_0207.PNG watermark.png -gravity center ./IMG_0207.PNG
+convert -composite ./IMG_0208.PNG watermark.png -gravity center ./IMG_0208.PNG
+convert -composite ./IMG_0209.PNG watermark.png -gravity center ./IMG_0209.PNG
+convert -composite ./IMG_0210.PNG watermark.png -gravity center ./IMG_0210.PNG
+convert -composite ./IMG_0213.PNG watermark.png -gravity center ./IMG_0213.PNG
+convert -composite ./IMG_0214.PNG watermark.png -gravity center ./IMG_0214.PNG
+convert -composite ./IMG_0215.PNG watermark.png -gravity center ./IMG_0215.PNG
+convert -composite ./IMG_0216.PNG watermark.png -gravity center ./IMG_0216.PNG
+convert -composite ./IMG_0217.PNG watermark.png -gravity center ./IMG_0217.PNG
+convert -composite ./IMG_0224.PNG watermark.png -gravity center ./IMG_0224.PNG
+convert -composite ./IMG_0225.PNG watermark.png -gravity center ./IMG_0225.PNG
+convert -composite ./pro/IMG_0193_pro.PNG watermark.png -gravity center ./pro/IMG_0193_pro.PNG
+convert -composite ./pro/IMG_0194_pro.PNG watermark.png -gravity center ./pro/IMG_0194_pro.PNG
+convert -composite ./pro/IMG_0195_pro.PNG watermark.png -gravity center ./pro/IMG_0195_pro.PNG
+convert -composite ./pro/IMG_0206_pro.PNG watermark.png -gravity center ./pro/IMG_0206_pro.PNG
+convert -composite ./pro/IMG_0215_pro.PNG watermark.png -gravity center ./pro/IMG_0215_pro.PNG
+
+
+
+
+
